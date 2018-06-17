7a3ec790e368d2df7c7d77c3b2cab9d78c4c6735
edge535 <1204565324@qq.com>
2018-06-17 23:17:11 +0800

    做动画之前保存

--- 人物介绍/js/人物样式.js
+++ 人物介绍/js/人物样式.js
@@ -19,33 +19,45 @@
 //          ┗━┻━┛   ┗━┻━┛
 //
 
-$(function(){
-console.log(
-//      ┏┛┻━━━━┛┻┓
-//      ┃　　　　     　┃
-//      ┃　   ━  　　  ┃
-//      ┃ ┳┛  ┗┳    ┃
-//      ┃　　　　　　    ┃
-//      ┃　　　┻　　    ┃
-//      ┃　　　　　　    ┃
-//      ┗━┓　 　┏━━━┛
-//        ┃　　　┃   神兽保佑
-//        ┃　　　┃   代码无BUG！
-//        ┃　　　┗━━━━━━━━━┓
-//        ┃　　　　　　　    ┣┓
-//        ┃　　　　         ┏┛
-//        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
-//          ┃ ┫ ┫   ┃ ┫ ┫
-//          ┗━┻━┛   ┗━┻━┛
-//
-)
-});
 
-$('.person').on({
-    mouseover:function(){
-        $(this).css('transform','scale(1.2)')
-    },
-    mouseout:function(){
-        $(this).css('transform','scale(1)')
-    }
-    });
+
+$(function(){
+    console.log(
+      `      ┏┛┻━━━━┛┻┓
+      ┃　　　　     　┃
+      ┃　   ━  　   ┃
+      ┃ ┳┛  ┗┳   ┃
+      ┃　　　　　　   ┃
+      ┃　　　┻　　   ┃
+      ┃　　　　　　   ┃
+      ┗━┓　 ┏━━━┛
+        ┃　　　┃   神兽保佑
+        ┃　　　┃   代码无BUG！
+        ┃　　　┗━━━━━━━━━┓
+        ┃　　　　　　　           ┣┓
+        ┃　　　　                 ┏┛
+        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+            ┃ ┫ ┫     ┃  ┫┫
+            ┗━┻┛     ┗━┻┛`
+    )
+});/*召唤神兽*/
+
+$('.person').on({/*给人物框添加事件*/
+    mouseover: function () {/*鼠标进入事件*/
+        $(this).css('transform','scale(1.2)');
+        $(this).find('.halo').css({
+            border: 'solid 1px',
+            background: 'url("../img/result/ceshi.png")',
+            backgroundPosition: "-28px -33px"
+        })
+    },/*鼠标进入事件*/
+
+    mouseout:function(){/*移出还原*/
+        $(this).css('transform','scale(1)');
+        $(this).find('.halo').css({
+            border: 'solid 1px',
+            background: 'url("../img/result/jd-85-33.png")'
+        })
+    }/*移出还原*/
+
+});/*给人物框添加事件*/
