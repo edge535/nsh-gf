41ce0525c8996226c6b0c6bab50adf03404ffba1
edge535 <1204565324@qq.com>
2018-06-19 17:55:09 +0800

    暂存

--- 人物介绍/js/运与跳转.js
+++ 人物介绍/js/运与跳转.js
@@ -20,20 +20,31 @@
 
 
 function yun_ani(){/*云动画*/
+    $('.yun').css('display','block');
     $('.yun').css('animation','yunIn 3s steps(28)');/*云进入*/
     setTimeout(function(){/*云退出*/
         $('.yun').css('animation','yunOut 3s steps(28)');
-    },3500)/*云退出*/
+    },3000)/*云退出*/
+    setTimeout(function(){
+        $('.yun').css('display','none');
+    },6000)
 }/*云动画*/
 
+/*跳转*/
 $('.person_all').find('a').click(function(){/*点击之后把云层显示，并执行动画*/
-    $('.yun').css('display','block');
-    yun_ani()
+    yun_ani();
+    setTimeout(function(){
+        $('#box').css('display','none');
+    },3000);
+    let index = $(this).parent().attr('id');
+    $('.xq').find('div').each(function(){
+        if($(this).attr('class') == index){
+            $(this).css('display','block');
+
+        }
+    });
+    // $('.xq').find(`#${index}`).css('display','block');
 });/*点击之后把云层显示，并执行动画*/
 
 
 
-/*跳转*/
-$('#box').click(function(){
-
-});
