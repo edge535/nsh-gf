5df68d93e6f6712033c3b254e698f515dc6e4216
edge535 <1204565324@qq.com>
2018-06-20 11:29:25 +0800

    20180620人物介绍完成

--- 人物介绍/js/运与跳转.js
+++ 人物介绍/js/运与跳转.js
@@ -36,39 +36,48 @@ function yun_ani(){/*云动画*/
 
 /*跳转*/
 $('.person_all').find('a').click(function(){/*点击之后把云层显示，并执行动画*/
-    $('.xq').css('display','block');
-    yun_ani();
-    setTimeout(function(){
-        $('#box').css('display','none');
-    },3000);
-    let index = $(this).parent().attr('id');
-    console.log(index);
-    $('.xq').find('div').each(function(){
-        if($(this).attr('class') == index){
-            console.log($(this));
-            $(this).css({
-                display:'block',
-                zIndex:100
-            });
-        }
-    });
+
+    let index = $(this).parent().attr('id');/*获取到点击的index*/
+
+
+
+    if(index == 'person_4' || index == 'person_9' || index == 'person_10'){
+    }else{
+        $('.xq').css('display','block');/*详情页显示*/
+        yun_ani();/*执行动画*/
+        setTimeout(function(){/*在云完全覆盖的时候box消失*/
+            $('#box').css('display','none');
+        },3000);/*在云完全覆盖的时候box消失*/
+
+        $('.xq').find('div').each(function(){/*所有的详情页*/
+            $(this).css('zIndex','-1')
+        });/*所有的详情页*/
+
+        $('.xq').find('div').each(function(){/*匹配index*/
+            if($(this).attr('class') == index){
+                $(this).css({
+                    // display:'block',
+                    zIndex:100
+                });
+            }
+        });
+    }
+
 });/*点击之后把云层显示，并执行动画*/
 
 
 $('.xq>div>a').click(function(){
-    // $('#pt').css('display','none');
     setTimeout(function(){
-
         $(this).parent().css({
-            display:'none',
-            zIndex:'-99999999'
+            // display:'none',
+            zIndex:'-2'
         });
         $('.xq').css('display','none');
     },3000);
     yun_ani();
     setTimeout(function(){
         $('#box').css('display','block');
-        $(this).parent().css('display','none');
+        // $(this).parent().css('display','none');
     },3000);
 
 });
